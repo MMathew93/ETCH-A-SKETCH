353ffcf9597e6402dc10fae632feba84c814c6ce
lyndexer <mckinney.m93@gmail.com>
2020-01-14 21:18:18 -0800

    added more JS and updated the readme.

---
+++ README.md
@@ -0,0 +1,3 @@
+Etch-A-Sketch project
+01/12/2019 04:30pm
+I started the REAL barebones of this. I have an idea of what I want to do but I am npt entirely sure on how to manipulate the DOM how I want it too. I nee to re-read the chapter and go back before I mindlessly try to google an answer.
---
+++ myscript.js
@@ -0,0 +1,30 @@
+const container= document.querySelector('#container');
+
+const row= document.querySelector('div');
+
+const column= document.querySelector('div');
+
+const cell= documents.querySelector('div');
+
+//creates a default grid sized 16x16
+function grid() {
+    row(16);
+    column(16;)
+};
+
+
+//functions to change row and column
+function gridSize(row, column) {
+};
+
+function rowSize() {
+    for(let i= 0; i<row; i++) {
+        //update row in div
+    }
+};
+
+function columnSize() {
+    for(let i=0; i<column; i++) {
+        //update column in div
+    }
+};
