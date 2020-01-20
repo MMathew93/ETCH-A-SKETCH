cd79cb91afcd9d45e7e468e1799328177c59b816
lyndexer <mckinney.m93@gmail.com>
2020-01-19 16:19:43 -0800

    grid is online, attempting to get clear and resize button to work

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 Etch-A-Sketch project
 01/12/2019 04:30pm
 I started the REAL barebones of this. I have an idea of what I want to do but I am npt entirely sure on how to manipulate the DOM how I want it too. I nee to re-read the chapter and go back before I mindlessly try to google an answer.
+
+
+01/14/2019 09:15pm
+I have been studying DOM stuff more and more, I have a much better understanding then when I first began, but I'm still not fully grasping it.
+
+WOrked on some of the inner coding but it isn't working, I'm missing something, unsure of how obvious it is or not just yet.
--- myscript.js
+++ myscript.js
@@ -1,30 +1,50 @@
-const container= document.querySelector('#container');
+//important variables
+const container= document.getElementById("container");
+let rows= document.getElementsByClassName("gridRow");
+let cells= document.getElementsByClassName("cell");
+const button= document.getElementById("reset");
 
-const row= document.querySelector('div');
-
-const column= document.querySelector('div');
-
-const cell= documents.querySelector('div');
-
+//reset and resize button
+button.addEventListener("click", clearAndResize);
+   
+   
+grid();
 //creates a default grid sized 16x16
 function grid() {
-    row(16);
-    column(16;)
+    newRow(16);
+    newColumn(16);
+}
+
+
+//functions to take (row, column)
+//updates and creates rows within the container Div
+function newRow(rowNum) {
+    //loops and pushes rows till complete
+    for(r= 0; r< rowNum; r++) {
+        let row= document.createElement("div");
+        container.appendChild(row).className= "gridRow";
+    };
 };
 
-
-//functions to change row and column
-function gridSize(row, column) {
+//update and creates columns
+//loops to match rows first, then inputs cells
+function newColumn(cellNum) {
+    for(p= 0; p< rows.length; p++) {
+        for(c= 0; c< cellNum; c++) {
+            let newCell= document.createElement("div");
+            rows[c].appendChild(newCell).className= "cell";              
+        };        
+    };
 };
 
-function rowSize() {
-    for(let i= 0; i<row; i++) {
-        //update row in div
+function clearAndResize() {
+    const input= prompt("Enter a new grid size, up to 100!");
+    if(input< 1 || input>100) {
+        return;    
+    }else{ 
+        gridRow.parentNode.removeChild(gridRow);
     }
+    newRow(input);
+    newColumn(input);S
 };
 
-function columnSize() {
-    for(let i=0; i<column; i++) {
-        //update column in div
-    }
-};
