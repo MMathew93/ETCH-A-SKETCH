ed922aab845e7d14d998a4559619ed78882f236c
lyndexer <mckinney.m93@gmail.com>
2020-01-22 19:51:50 -0800

    FInal pass through on project, I got buttons working for color changing as well as fixed the prompt loop on cancel or empty inputs

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ Worked on some of the inner coding but it isn't working, I'm missing something, 
 
 01/19/2020
 I put off working on this project for quite some time till the weekend. I have made loads of hedway after giving myself time to re-study the DOM and get a better understanding of it. I have a lot done, I am still working on prettying it up some more but the basics are there. The grid size is still wonky, I need to fix the container as it it just filling the entire webpage with cells instead of confining them into one container size.
+
+01/20/2020
+Got the grid and semi working cancel option to work correctly. Next is colors and oevrall prettyness of web page.
--- myscript.js
+++ myscript.js
@@ -12,8 +12,8 @@ grid();
 function grid() {
     newRow(16);
     newColumn(16);
-}
-
+    cellSizing(16);
+};
 
 //functions to take (row, column)
 //updates and creates rows within the container Div
@@ -31,9 +31,22 @@ function newColumn(cellNum) {
     for(p= 0; p< rows.length; p++) {
         for(c= 0; c< cellNum; c++) {
             let newCell= document.createElement("div");
-            rows[c].appendChild(newCell).className= "cell";              
+            rows[c].appendChild(newCell).className= "cell";  
         };        
     };
+};
+
+function cellSizing(size) {
+    var x = rows;
+    var y= cells;
+    var i;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.height= (900/size) + "px";
+    }
+    for (i = 0; i < y.length; i++) {
+      y[i].style.width= (900/size) + "px";
+      y[i].style.height= (900/size) + "px";
+};
 };
 
 //hover-color in cell
@@ -45,8 +58,8 @@ container.addEventListener("mouseover", fillCell);
 function fillCell(e) {
     if(e.target.className=== "cell") {
         e.target.style.background= "black";
-    }
-}
+    };
+};
 
 //stopFill function
 //function stopFill(e) {}
@@ -54,14 +67,18 @@ function fillCell(e) {
 //resets entire grid and resizes
 function clearAndResize() {
     const input= prompt("Enter a new grid size, up to 100!");
-    if(input< 1 || input>100) {
-        return;    
-    }else{ 
+    if(input< 1 || input>100 || isNaN(input) && input!== '') {
+        alert("Invalid size, try again");
+        clearAndResize();
+    }else if(input=== '') {
+        return;
+    }else{
         while(container.firstChild) {
             container.removeChild(container.firstChild);
         }
-    }
-    newRow(input);
-    newColumn(input);
+        newRow(input);
+        newColumn(input);
+        cellSizing(input);
+    };
 };
 
