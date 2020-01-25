480d83236f6b687ce26b3b583059d0d6f3368253
lyndexer <mckinney.m93@gmail.com>
2020-01-24 18:23:35 -0800

    Project is complete and final for now!

--- README.md
+++ README.md
@@ -11,5 +11,10 @@ Worked on some of the inner coding but it isn't working, I'm missing something, 
 01/19/2020
 I put off working on this project for quite some time till the weekend. I have made loads of hedway after giving myself time to re-study the DOM and get a better understanding of it. I have a lot done, I am still working on prettying it up some more but the basics are there. The grid size is still wonky, I need to fix the container as it it just filling the entire webpage with cells instead of confining them into one container size.
 
+
 01/20/2020
 Got the grid and semi working cancel option to work correctly. Next is colors and oevrall prettyness of web page.
+
+
+01/22/2020
+It is finished! I got the additional buttons fixed for color changing as well as fixing the prompt loop issue, CHAIN OF COMMAND! Had to swap them in order to fix it. This was a real headache without some help from my buddy Rob, he allowed me to re-think some of my code and allowed me to explain it out loud so not only he understood it but me as well!
--- myscript.js
+++ myscript.js
@@ -3,12 +3,61 @@ const container= document.getElementById("container");
 let rows= document.getElementsByClassName("gridRow");
 let cells= document.getElementsByClassName("cell");
 const button= document.getElementById("reset");
+const colorPicker= document.getElementById("colorChange");
+const button2= document.getElementById("rainbow");
+let colorMode= 'black';
 
 //reset and resize button
 button.addEventListener("click", clearAndResize);
+//resets entire grid and resizes
+function clearAndResize() {
+    const input= prompt("Enter a new grid size, up to 100!");
+    if(input== null||input== "") {
+        return;
+    }else if(input< 1 || input>100 || isNaN(input)) {
+            alert("Invalid size, try again");
+            clearAndResize();
+    }else{
+        while(container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+        newRow(input);
+        newColumn(input);
+        cellSizing(input);
+    };
+};
 
-grid();
+//color buttons
+colorPicker.addEventListener("click", colorFillChange);
+//pulls up color Input and changes based on user input
+function colorFillChange() {
+    colorMode= "colorValue";
+};
+
+button2.addEventListener("click", colorFillRainbow);
+//changes color Input to be random/rainbow
+function colorFillRainbow() {
+    colorMode= "random";
+}
+
+//hover-color in cell, default black
+container.addEventListener("mouseover", fillCell);
+//fill Function
+function fillCell(e) {
+    if(e.target.className=== "cell") {
+        if(colorMode=== "random") {
+            e.target.style.background= `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
+        }else if(colorMode=== "colorValue") {
+            const colorValue= document.getElementById("colorChange").value;
+            e.target.style.background= `${colorValue}`; 
+        }else {
+            e.target.style.background= "black"  
+    };
+};
+};
+
 //creates a default grid sized 16x16
+grid();
 function grid() {
     newRow(16);
     newColumn(16);
@@ -42,43 +91,12 @@ function cellSizing(size) {
     var i;
     for (i = 0; i < x.length; i++) {
       x[i].style.height= (900/size) + "px";
-    }
+    };
     for (i = 0; i < y.length; i++) {
       y[i].style.width= (900/size) + "px";
       y[i].style.height= (900/size) + "px";
-};
-};
-
-//hover-color in cell
-container.addEventListener("mouseover", fillCell);
-//cells.addEventListener("click", stopFillCell);
-//container.addEventListener("click", stopFill);
-
-//fill Function
-function fillCell(e) {
-    if(e.target.className=== "cell") {
-        e.target.style.background= "black";
     };
 };
 
-//stopFill function
-//function stopFill(e) {}
 
-//resets entire grid and resizes
-function clearAndResize() {
-    const input= prompt("Enter a new grid size, up to 100!");
-    if(input< 1 || input>100 || isNaN(input) && input!== '') {
-        alert("Invalid size, try again");
-        clearAndResize();
-    }else if(input=== '') {
-        return;
-    }else{
-        while(container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-        newRow(input);
-        newColumn(input);
-        cellSizing(input);
-    };
-};
 
